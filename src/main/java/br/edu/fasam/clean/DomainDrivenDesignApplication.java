9e993ba0234416aa793dce5a8c4382c2fc26a123
Bruno Urbano Rodrigues <brunourb@gmail.com>
2021-09-15 21:21:18 -0300

    Primeir commit

---
+++ src/main/java/br/edu/fasam/clean/DomainDrivenDesignApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fasam.clean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainDrivenDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainDrivenDesignApplication.class, args);
+	}
+
+}
